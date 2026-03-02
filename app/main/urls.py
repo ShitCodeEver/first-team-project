9d43acca86ba96b9bdb9eaa17832060751d28bd1
ShitCodeEver <romapavlov31@gmail.com>
2026-03-02 10:24:27 +0500

    карта товара + отображение

--- app/main/urls.py
+++ app/main/urls.py
@@ -5,8 +5,9 @@ from . import views
 app_name = 'main'
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name = 'index'),
-    path('catalog/', views.CatalogView.as_view(), name= 'catalog'),
-    path('catalog/<slug:category_slug>', views.ProductDetailView.as_view(), name = 'product_detail'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('catalog/', views.CatalogView.as_view(), name='catalog_all'),
+    path('catalog/<slug:category_slug>/', views.CatalogView.as_view(), name='catalog'),
+    path('catalog/<slug:category_slug>/<slug:slug>/', views.ProductDetailView.as_view(), name='product'),
     path('about/', views.AboutView.as_view(), name='about_page'),
 ]
