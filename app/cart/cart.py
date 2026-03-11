dbd365aa6e3c452cee2b4cb62c8b2540e7984bc2
ShitCodeEver <romapavlov31@gmail.com>
2026-03-11 23:25:55 +0500

    приложение cart и логика cart

---
+++ app/cart/cart.py
@@ -0,0 +1,79 @@
+from decimal import Decimal
+from django.shortcuts import get_object_or_404
+from main.models import Product, ProductSize
+
+class Cart:
+    def __init__(self, request):
+        self.session = request.session
+        cart = self.session.get("cart")
+        if not cart:
+            cart = self.session["cart"] = {}
+        self.cart = cart
+        
+    def add(self, product, quatity=1, override_quatity=False):
+        product_id = str(product.id)
+        size_name = str(product.productsize)
+        
+        cart_key = f"{product_id}_{size_name}"
+        
+        if cart_key not in self.cart:
+            self.cart[cart_key] = {
+                'quantity': 0,
+                'price' : str(product.productsize.price),
+                'product_id' : product_id,
+                'size' : size_name
+            }
+        if override_quatity:
+            self.cart[cart_key]['quantity'] = override_quatity
+        else:
+            self.cart[cart_key]['quantity'] += quatity
+            
+    def save(self):
+        self.session.modified = True
+        
+    def remove(self, product, size):
+        del self.cart[cart_key]
+        self.save
+        
+    def update(self, product, size, quantity):
+        if quantity <=0:
+            self.remove(product, size)
+        else:
+            self.add(product, size, quantity, override_quatity=True)
+            
+    def __iter__(self):
+        product_ids = [item['product_id'] for item in self.cart.values()]
+        products = Product.objects.filter(id__in = product_ids)
+        cart = self.cart.copy()
+        
+        for product in products:
+            for cart_key, cart_item in cart.items():
+                if cart_item['product_id'] == str(product.id):
+                    cart_item['product'] = product
+                    cart_item['total_price'] = Decimal(cart_item['price']) * cart
+                    yield cart_item
+                    
+    def __len__(self):
+        lenght = sum(item['quantity'] for item in self.cart.values())
+        return lenght
+    
+    def get_total_price(self):
+        total = sum(Decimal(item['price']) * item['quantity']
+                    for item in self.cart.values())
+        return total
+    
+    def clear(self):
+        del self.session['cart']
+        self.save()
+        
+    def get_cart_items(self):
+        items = []
+        for item in self:
+            items.append({
+                'product' : item['product'],
+                'quantity' : item['quntity'],
+                'size' : item['size'],
+                'price' : Decimal(item['price']),
+                'total_price' : item['total_price'],
+                'cart_key': f"{item['product_id']}_{item['size']}"
+            })
